1f7fb1731fbbb4bb8ed92fa0985dd86700124516
KabirKhanuja <kabirkhanuja@gmail.com>
2025-11-02 14:57:30 +0530

    Reformatted global.css

--- apps/web/.next/static/chunks/My_drive_VIT_SY_SEM1_EDI_clause-genie_apps_web_src_app_page_tsx_0b0312d2._.js
+++ apps/web/.next/static/chunks/My_drive_VIT_SY_SEM1_EDI_clause-genie_apps_web_src_app_page_tsx_0b0312d2._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/My_drive_VIT_SY_SEM1_EDI_clause-genie_bf10b6fb._.js"
+  "static/chunks/My_drive_VIT_SY_SEM1_EDI_clause-genie_7e620ed8._.js"
 ],
     source: "dynamic"
 });
